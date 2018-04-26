7326a1e27815e1461d59de242eb0d8f5acf69ec0
bobowang2017 <409879654@qq.com>
2018-04-26 18:09:45 +0800

    commit

--- activity_info/views.py
+++ activity_info/views.py
@@ -8,3 +8,14 @@ class ActivityInfoView(mixins.CreateModelMixin, viewsets.GenericViewSet, mixins.
                        mixins.RetrieveModelMixin, mixins.UpdateModelMixin):
     queryset = ActivityInfo.objects.all()
     serializer_class = ActivityInfoSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        param = self.request.query_params
+        _id = param.get('id')
+        _type = param.get('type')
+        if _id:
+            queryset = queryset.filter(id=_id)
+        if _type:
+            queryset = queryset.filter(type=_type)
+        return queryset

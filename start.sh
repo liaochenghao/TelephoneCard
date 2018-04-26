e3f01de0f11ba862fd6a11320b7a0ffbaa6a8172
bobowang2017 <409879654@qq.com>
2018-04-26 11:37:53 +0800

    commit

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ps -aux | grep 8003 | awk '{print $2}' | xargs kill -9
-gunicorn TelephoneCard.wsgi.application -b 0.0.0.0:8003 -w 4 -t 300 --reload
+ps -aux | grep 8001 | awk '{print $2}' | xargs kill -9
+gunicorn TelephoneCard.wsgi.application -b 0.0.0.0:8001 -w 4 -t 300 --reload
